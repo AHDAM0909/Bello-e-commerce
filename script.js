75c3906ef6074a1f825076415d17426c89d867aa
unknown <belloahdamkehinde@gmail.com>
2026-01-01 17:05:14 -0600

    update the harmburger

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ menulink.addEventListener("click", function(){
   allLink.classList.toggle("active")
  
 })
+let button = document.querySelector("#btn")
+button.addEventListener("mouseover", function(){
+ const isActive = button.classList.toggle("active");
+  // button.textContent = button.classList.contains("active")?"Selected":"Shop Now"
+  button.textContent = isActive ? "Selected" :  " Shop Now"
+})
+let buttonTwo = document.querySelector("#flash-SaleBtn")
+buttonTwo.addEventListener("mouseover", function(){
+  const isActiveTwo = buttonTwo.classList.toggle("active");
+  buttonTwo.textContent = isActiveTwo ? "Explore" : "Shop Now"
+})
+let buttonThree = document.querySelector("#btnThree")
+buttonThree.addEventListener("mouseover", function(){
+  const isActiveThree = buttonThree.classList.toggle("active");
+  buttonThree.textContent = isActiveThree ? "Explore" : "Shop Now"
+})
